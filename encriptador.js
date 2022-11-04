188011a69f88d1e141790c0c4de4c4f0ae3b4599
Katty Montenegro Flórez <110270063+KenkoxD@users.noreply.github.com>
2022-11-03 23:18:23 -0300

    initial commit

---
+++ encriptador.js
@@ -0,0 +1,46 @@
+// Boton Encriptar
+    function encriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+    document.getElementById("btnCopiar").style.display = "show";
+    document.getElementById("btnCopiar").style.display = "inherit";
+
+    alert("El texto se ha Encriptado");
+
+    
+}
+
+// Boton Desencriptar
+    function desencriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+
+    alert("El texto se ha Desencriptado")
+
+    
+}
+
+// Boton Copiar
+    function copiar(){
+    var contenido = document.querySelector("#textoDesencriptado");
+    contenido.select();
+    document.execCommand("copy");
+    
+    alert("El texto se ha Copiado");
+
+}
+
